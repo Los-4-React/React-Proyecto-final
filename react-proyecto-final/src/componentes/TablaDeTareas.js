deebb056dff4fad7a936212260acaaec9d658563
Lu <luciaverge12@hotmail.com>
2023-11-24 12:05:47 -0300

    agrego tabla de tareas

---
+++ react-proyecto-final/src/componentes/TablaDeTareas.js
@@ -0,0 +1,30 @@
+import { FilaDeTareas } from "./FilaDeTareas";
+
+
+export const TablaDeTareas = ({tareas, alternarTarea, showCompleted = false}) => {
+
+    const filasDeTareas = (donevalue) =>{
+
+        return(
+            tareas
+            .filter(tarea => tarea.done === donevalue )
+            .map(tarea =>(
+                <FilaDeTareas tarea={tarea} key={tarea.name} alternarTarea={alternarTarea}/>
+            ))
+        )
+    }
+    return(
+        <table>
+            <thead>
+                <tr>
+                    <th>Tareas</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    filasDeTareas(showCompleted)
+                }
+            </tbody>
+        </table>
+    )
+}

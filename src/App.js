317c9261b29c582c132ab5c3baf63f21233438bb
Hernan <hernanferrero1@gmail.com>
2023-11-27 10:36:37 -0300

    subida al repo - Creador-de-tareas - en App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import "./App.css";
+import { CreadorDeTareas } from './componentes/CreadorDeTareas';
 import { TablaDeTareas } from './componentes/TablaDeTareas';
 import { ControlesVisibles } from './componentes/ControlesVisibles';
 
---
+++ src/componentes/CreadorDeTareas.js
@@ -0,0 +1,29 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { useState } from "react";
+
+
+export function CreadorDeTareas({crearTarea}) {
+
+    //captura lo que escribimos en el input
+    const [newTareaNombre, setNewTareaNombre] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        crearTarea(newTareaNombre);
+        setNewTareaNombre('');
+    };
+
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                name="tarea"
+                type="text"
+                placeholder="Ingresa una nueva tarea"
+                value={newTareaNombre}
+                onChange={(e) => setNewTareaNombre(e.target.value)} />
+                {" "}
+            <button>Guardar</button>
+        </form>
+    );
+}
